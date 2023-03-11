7a22f192129c036cc428081a5315c5fcb3e45d34
AbhayTr <53339132+AbhayTr@users.noreply.github.com>
2023-03-11 18:25:57 +0530

    Changed BB Script.

--- projects/bbitems.js
+++ projects/bbitems.js
@@ -1,11 +1,8 @@
-items = document.getElementsByClassName("uiv2-quantity-price");
+items = document.getElementsByClassName("Label-sc-15v1nk5-0 gJxZPQ text-darkOnyx-800 leading-sm font-regular text-base");
 total_items = 0;
 for (item = 0; item < items.length; item++)
 {
-    item_quantity = items[item].innerHTML;
-    if (items[item].parentElement.parentElement.className == "uiv2-griditems-offers" || items[item].parentElement.parentElement.className == "uiv2-griditems-details")
-    {
-      total_items += parseInt(item_quantity);
-    }
+    item_quantity = items[item].innerText.replace("Qty : ", "");
+    total_items += parseInt(item_quantity);
 }
 alert("There are " + total_items + " items in this order");
